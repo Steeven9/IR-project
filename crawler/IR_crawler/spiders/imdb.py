0768219412c13e7d349f488f02adec109318dd0c
Stefano Taillefert <stefano.taille@gmail.com>
2020-11-09 13:23:38 +0100

    Add first IMDB crawler

---
+++ crawler/IR_crawler/spiders/imdb.py
@@ -0,0 +1,37 @@
+import scrapy
+
+SEARCH_QUERY = 'https://www.imdb.com/search/title/?release_date=2010-01-01,2020-12-31'
+
+class ImdbSpider(scrapy.Spider):
+    name = 'imdb'
+    allowed_domains = ['imdb.com']
+    start_urls = [SEARCH_QUERY]
+
+    def parse(self, response):
+        movies = response.css('.lister-item')
+        for movie in movies:
+            year = movie.css('.lister-item-year::text').extract_first() or None
+            genre = movie.css(".genre::text").extract_first() or None
+            description = movie.css('.lister-item-content p:nth-child(4)::text').extract_first() or None
+
+            if year is not None:
+                year = year[1:-1].strip()
+
+            if genre is not None:
+                genre = genre.strip().split(', ')
+
+            if description is not None:
+                description = description.strip()
+
+            yield {
+                'title': movie.css('.lister-item-header a::text').extract_first(),
+                'rating': movie.css('.ratings-imdb-rating strong::text').extract_first(),
+                'year': year,
+                'img_url': movie.css('.lister-item-image a img::attr(src)').extract_first(),
+                'genre': genre,
+                'description': description
+            }
+
+        next_page = response.css('.next-page::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
